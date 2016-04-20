f4ec8770d2930fc54a8926b6b554dd74130ea864
Sven <sven.rosenzweig@t-online.de>
2016-04-20 14:08:02 +0200

    Add AddTask Activity to the app navigation

--- TaskLoc/app/src/main/java/de/unima/pc2016/taskloc/application/activities/StartActivity.java
+++ TaskLoc/app/src/main/java/de/unima/pc2016/taskloc/application/activities/StartActivity.java
@@ -2,8 +2,11 @@ package de.unima.pc2016.taskloc.application.activities;
 
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -11,10 +14,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import de.unima.pc2016.taskloc.AddNewTask;
 
 import de.unima.pc2016.taskloc.R;
 import de.unima.pc2016.taskloc.application.database.DataSource;
@@ -25,10 +31,13 @@ public class StartActivity extends AppCompatActivity{
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private FloatingActionButton fbAddAction;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.context = this.getApplicationContext();
         setContentView(R.layout.activity_start);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -39,6 +48,9 @@ public class StartActivity extends AppCompatActivity{
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+        fbAddAction = (FloatingActionButton) findViewById(R.id.fabAddTask);
+        fbAddAction.setOnClickListener(new AddNewTaskListener());
 
     }
 
@@ -76,6 +88,15 @@ public class StartActivity extends AppCompatActivity{
         @Override
         public CharSequence getPageTitle(int position) {
             return mFragmentTitleList.get(position);
+        }
+    }
+
+    public class AddNewTaskListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(context, AddNewTask.class);
+            startActivity(intent);
         }
     }
 
