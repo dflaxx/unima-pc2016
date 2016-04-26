4b747dd20379bd80e2bd2d40b26e041a81a3822e
Ümit Tepe <uemit.tepe@students.uni-mannheim.de>
2016-04-26 19:06:46 +0200

    Added method for notification - DEMO

--- TaskLoc/app/src/main/java/de/unima/pc2016/taskloc/application/activities/StartActivity.java
+++ TaskLoc/app/src/main/java/de/unima/pc2016/taskloc/application/activities/StartActivity.java
@@ -2,12 +2,16 @@ package de.unima.pc2016.taskloc.application.activities;
 
 
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -54,6 +58,9 @@ public class StartActivity extends AppCompatActivity {
         viewPager.setOnPageChangeListener(new FragmentChanged()); //Needed so the onStop and onResume methods are called
          this.geofenceController = GeofenceController.getInstance(this.getApplicationContext());
 
+
+        // Just for demo usage
+        this.createNotification("TestTask1");
     }
 
 
@@ -123,6 +130,42 @@ public class StartActivity extends AppCompatActivity {
 
         }
     }
+
+
+    public void createNotification(String task) {
+    //Placed here for demo usage
+        String title = "TaskLoc";
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_taskloc_launcher) //Icon shown in notification bar
+                        .setContentTitle(title) // Notification title
+                        .setContentText(task); // Message shown in notification bar
+// Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, StartActivity.class);
+
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+// Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(StartActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+        // (int) System.currentTimeMillis() used to create unique ID
+        mNotificationManager.notify((int) System.currentTimeMillis(), mBuilder.build());
+
+    }
+
 
 
 
