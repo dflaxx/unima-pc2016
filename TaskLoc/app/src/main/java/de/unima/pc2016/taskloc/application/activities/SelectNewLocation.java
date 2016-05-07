36dfa532ee391c521554eb501b14323162c7e3e6
Daniel Flachs <daniel.flachs@fim.uni-mannheim.de>
2016-05-07 17:50:59 +0200

    SelectNewLocation: Request current location and center the map accordingly.

--- TaskLoc/app/src/main/java/de/unima/pc2016/taskloc/application/activities/SelectNewLocation.java
+++ TaskLoc/app/src/main/java/de/unima/pc2016/taskloc/application/activities/SelectNewLocation.java
@@ -2,7 +2,10 @@ package de.unima.pc2016.taskloc.application.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -10,6 +13,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -20,7 +24,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import de.unima.pc2016.taskloc.R;
 import de.unima.pc2016.taskloc.application.database.DataSource;
 //ToDo: Title muss noch gesetzt werden
-public class SelectNewLocation extends FragmentActivity implements OnMapReadyCallback {
+public class SelectNewLocation extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
     private Location newLocation;
@@ -29,6 +33,11 @@ public class SelectNewLocation extends FragmentActivity implements OnMapReadyCal
 
     private EditText txtLocationName;
     private String locationName;
+
+    private Location currLocation;
+    private LatLng currPosition;
+    private LocationManager locationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,7 +49,7 @@ public class SelectNewLocation extends FragmentActivity implements OnMapReadyCal
 
 
         newLocation = new Location("");
-        this.context = context;
+        this.context = this.getApplicationContext();
         locationName = "Test_Location";
         this.addLocation = new AsyncTaskAddLocation();
         this.txtLocationName = (EditText) findViewById(R.id.txtLocationName);
@@ -65,6 +74,23 @@ public class SelectNewLocation extends FragmentActivity implements OnMapReadyCal
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        getAndSetLocation();
+
+        if (context.checkCallingOrSelfPermission("android.permission.ACCESS_FINE_LOCATION")
+                        == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true); //Enable the location button of the GMaps API and display the blue location marker.
+        }
+
+        // Move camera to current location, if available.
+        if (currPosition != null) {
+            mMap.addMarker(new MarkerOptions().position(currPosition).title("Aktuelle Position"));
+            //mMap.moveCamera(CameraUpdateFactory.newLatLng(currPosition));
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(currPosition, 12);
+
+            mMap.animateCamera(cameraUpdate);
+        }
+
         // Setting a click event handler for the map
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
 
@@ -108,6 +134,102 @@ public class SelectNewLocation extends FragmentActivity implements OnMapReadyCal
     public void onDestroy(){
         super.onDestroy();
         finish();
+
+    }
+
+    private void getAndSetLocation() {
+        Log.d("MOF - getAndSetLocation",
+                "method called");
+
+        try {
+            locationManager = (LocationManager) context
+                    .getSystemService(Context.LOCATION_SERVICE);
+
+            // getting GPS status
+            boolean isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // getting network status
+            boolean isNetworkEnabled = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            Log.d("MOF - getAndSetLocation",
+                    "isGPSEnabled = " + isGPSEnabled + ", "
+                            + "isNetworkEnabled = " + isNetworkEnabled);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                Log.d("MOF - getAndSetLocation",
+                        "No active provider found.");
+            } else {
+                //this.canGetLocation = true;
+                if (isNetworkEnabled &&
+                        context.checkCallingOrSelfPermission("android.permission.ACCESS_FINE_LOCATION")
+                                == PackageManager.PERMISSION_GRANTED) {
+                    locationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            1 * 60 * 1000, // 1 minute
+                            10,
+                            this);
+                    Log.d("Network", "Network Enabled");
+                    if (locationManager != null) {
+                        currLocation = locationManager
+                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (currLocation != null) {
+                            currPosition = new LatLng(currLocation.getLatitude(), currLocation.getLongitude());
+                            Log.d("MOF - getAndSetLocation",
+                                    "Network PosFix: "
+                                            + "(" + currPosition.latitude + "," + currPosition.longitude + ")");
+                        }
+                    }
+                }
+                // if GPS Enabled get lat/long using GPS Services
+                if (isGPSEnabled) {
+                    if (currLocation == null) {
+                        Log.d("MOF - getAndSetLocation",
+                                "Overriding Network PosFix by GPS...");
+
+                    }
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            1 * 60 * 1000, // 1 min
+                            10,
+                            this);
+                    Log.d("GPS", "GPS Enabled");
+                    if (locationManager != null) {
+                        currLocation = locationManager
+                                .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        if (currLocation != null) {
+                            currPosition = new LatLng(currLocation.getLatitude(), currLocation.getLongitude());                            Log.d("MOF - getAndSetLocation",
+                                    "GPS PosFix: "
+                                            + "(" + currPosition.latitude + "," + currPosition.longitude + ")");
+                        }
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
 
     }
 
